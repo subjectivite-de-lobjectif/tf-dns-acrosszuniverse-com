e43079edbce2acb21acba9d8fab8861a4c3b9ed1
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2020-02-09 15:25:53 +0000

    Instantiate the 'ovh-dns' submodule

---
+++ dns-records.tf
@@ -0,0 +1,18 @@
+locals {
+  zone = "zerowastesgp.fr"
+
+  dns_records = [
+  ]
+}
+
+module "dns_records" {
+  source = "./modules/ovh-dns"
+
+  ovh_application_key    = var.ovh_application_key
+  ovh_application_secret = var.ovh_application_secret
+  ovh_consumer_key       = var.ovh_consumer_key
+  ovh_endpoint           = var.ovh_endpoint
+
+  zone    = local.zone
+  records = local.dns_records
+}
