9b736e72f99ae5176b5ec13c945ee2193b84b852
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2020-02-16 16:18:12 +0000

    Import existing DNS records

--- dns-records.tf
+++ dns-records.tf
@@ -2,6 +2,138 @@ locals {
   zone = "acrosszuniverse.com"
 
   dns_records = [
+    {
+      name = "",
+      type = "NS",
+      ttl = 0,
+      targets = [
+        "dns110.ovh.net.",
+        "ns110.ovh.net.",
+      ]
+    },
+    {
+      name = "",
+      type = "MX",
+      ttl = 0,
+      targets = [
+        "100 mx3.mail.ovh.net.",
+      ]
+    },
+    {
+      name = "",
+      type = "MX",
+      ttl = 0,
+      targets = [
+        "1 mx1.mail.ovh.net.",
+      ]
+    },
+    {
+      name = "",
+      type = "MX",
+      ttl = 0,
+      targets = [
+        "5 mx2.mail.ovh.net.",
+      ]
+    },
+    {
+      name = "",
+      type = "A",
+      ttl = 3600,
+      targets = [
+        "185.199.108.153",
+        "185.199.109.153",
+        "185.199.110.153",
+        "185.199.111.153",
+      ]
+    },
+    {
+      name = "",
+      type = "TXT",
+      ttl = 0,
+      targets = [
+        "brave-ledger-verification=53cb21c94b6b25a160987cd57a960ca3ed72d6d90849bcfb28edb134c75ed644",
+      ]
+    },
+    {
+      name = "",
+      type = "TXT",
+      ttl = 0,
+      targets = [
+        "keybase-site-verification=1A3LESGM18RNR2U_rHeN84GnjOO9lcPsPYzcOwQ8en8",
+      ]
+    },
+    {
+      name = "",
+      type = "SPF",
+      ttl = 600,
+      targets = [
+        "\"v=spf1 include:mx.ovh.com ~all\"",
+      ]
+    },
+    {
+      name = "autoconfig",
+      type = "CNAME",
+      ttl = 0,
+      targets = [
+        "mailconfig.ovh.net.",
+      ]
+    },
+    {
+      name = "autodiscover",
+      type = "CNAME",
+      ttl = 0,
+      targets = [
+        "mailconfig.ovh.net.",
+      ]
+    },
+    {
+      name = "_github-challenge-subjectivite-de-lobjectif",
+      type = "TXT",
+      ttl = 0,
+      targets = [
+        "54ccf6d1f5",
+      ]
+    },
+    {
+      name = "imap",
+      type = "CNAME",
+      ttl = 0,
+      targets = [
+        "ssl0.ovh.net.",
+      ]
+    },
+    {
+      name = "mail",
+      type = "CNAME",
+      ttl = 0,
+      targets = [
+        "ssl0.ovh.net.",
+      ]
+    },
+    {
+      name = "pop3",
+      type = "CNAME",
+      ttl = 0,
+      targets = [
+        "ssl0.ovh.net.",
+      ]
+    },
+    {
+      name = "smtp",
+      type = "CNAME",
+      ttl = 0,
+      targets = [
+        "ssl0.ovh.net.",
+      ]
+    },
+    {
+      name = "www",
+      type = "CNAME",
+      ttl = 0,
+      targets = [
+        "acrosszuniverse.com.",
+      ]
+    },
   ]
 }
 
